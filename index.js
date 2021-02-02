c91ad9a4d19793aa1d53b1a75da9c49e3cc3f80c
Louisiane_Berlin <corentine.piette@gmail.com>
2021-02-02 16:35:32 +0100

    Without Bonus

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b) {
+      return a-b;
+    })
+    this.length=this.items.length
+  }
 
-  max() {}
+  get(pos) {
+    if (pos>this.length) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length==0) throw new Error('EmptySortedList');
+    return Math.max(...this.items);
+  }
+
+  min() {
+    if (this.length==0) throw new Error('EmptySortedList');
+    return Math.min(...this.items);
+  }
 
   sum() {}
 
   avg() {}
 }
 
+let aList = new SortedList([1,2,3], 3)
+console.log(aList)
 module.exports = SortedList;
